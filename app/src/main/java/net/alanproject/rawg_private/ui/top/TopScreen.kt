85bfa851ebb6860a402e1964def05988ff2cc020
alan-project <banana205@gmail.com>
2022-01-26 00:15:08 -0500

    fetching data and compose simple list

--- app/src/main/java/net/alanproject/rawg_private/ui/top/TopScreen.kt
+++ app/src/main/java/net/alanproject/rawg_private/ui/top/TopScreen.kt
@@ -1,11 +1,21 @@
 package net.alanproject.rawg_private.ui.top
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
+import net.alanproject.domain.model.list.Result
 import net.alanproject.rawg_private.ui.theme.Rawg_privateTheme
 
 
@@ -15,10 +25,44 @@ fun TopScreen(){
     val topGames = viewModel.topListState.value
     
     LazyColumn(){
-        items(topGames){games->
-            Text(text = games.name)
+        items(topGames){game->
+            GameList(game)
             
         }
+    }
+}
+
+@Composable
+fun GameList(game: Result) {
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        Row {
+            Image(
+                painter = rememberImagePainter(game.backgroundImage),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = game.name,
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+
     }
 }
 
