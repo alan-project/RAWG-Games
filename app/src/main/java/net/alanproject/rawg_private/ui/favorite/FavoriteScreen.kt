243445e9c02bd2e218bba90cb73e35786ee8d9d6
alan-project <banana205@gmail.com>
2022-02-08 00:12:58 -0500

    bottom_navigation - WIP

---
+++ app/src/main/java/net/alanproject/rawg_private/ui/favorite/FavoriteScreen.kt
@@ -0,0 +1,17 @@
+package net.alanproject.rawg_private.ui.favorite
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun FavoriteScreen() {
+    Box(contentAlignment = Alignment.Center,modifier = Modifier.fillMaxSize()) {
+        Text(text = "FAVORITE SCREEN",style = TextStyle(color = Color.White))
+    }
+}
---
+++ app/src/main/java/net/alanproject/rawg_private/ui/navigation/NavigationItem.kt
@@ -0,0 +1,65 @@
+package net.alanproject.rawg_private.ui.navigation
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import net.alanproject.rawg_private.R
+
+sealed class NavigationItem(var route: String, var icon: Int, var title: String) {
+    object Home : NavigationItem("home", R.drawable.ic_home, "Home")
+    object List : NavigationItem("list", R.drawable.ic_list, "List")
+    object Favorite : NavigationItem("movies", R.drawable.ic_favorite, "Favorite")
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.List,
+        NavigationItem.Favorite,
+    )
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.black),
+        contentColor = Color.White
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title) },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = false,
+                onClick = {
+                    navController.navigate(item.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
